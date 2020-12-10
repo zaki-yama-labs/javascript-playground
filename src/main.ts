d18d2af71b035cd64fdcea12a1d93904ba64b6e4
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2020-12-10 15:23:16 +0900

    template

---
+++ src/main.ts
@@ -0,0 +1,66 @@
+// array.forEach() 内でasync関数回したらどうなるのか
+
+// ref. https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach#forEach_expects_a_synchronous_function
+// > `forEach` はプロミスを待ちません
+
+function sleep(ms: number) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve(), ms);
+  });
+}
+
+// sample 1
+// const arr = [1, 2, 3, 4, 5];
+// (async () => {
+//   await arr.forEach(async (value) => {
+//     console.log('start: ', value);
+//     await sleep(1000);
+//     console.log('end: ', value);
+//   });
+
+//   console.log("finished");
+// })();
+// start:  1
+// start:  2
+// start:  3
+// start:  4
+// start:  5
+// finished
+// (1秒)
+// end:  1
+// end:  2
+// end:  3
+// end:  4
+// end:  5
+
+// sample 2
+// 回避策としてforEachでなく for .. of を使う方法
+// const arr = [1, 2, 3, 4, 5];
+// (async () => {
+//   for (const value of arr) {
+//     console.log('start: ', value);
+//     await sleep(1000);
+//     console.log('end: ', value);
+//   }
+
+//   console.log("finished");
+// })();
+
+// sample 3
+// ループ中のPromiseを一つずつ処理する必要がない場合
+// Promise.allで全部の終了を待つ
+const arr = [1, 2, 3, 4, 5];
+(async () => {
+  await Promise.all(
+    arr.map((value) => {
+      return new Promise(resolve => {
+        console.log("start: ", value);
+        sleep(1000);
+        console.log("end: ", value);
+        resolve();
+      });
+    })
+  );
+
+  console.log("finished");
+})();
